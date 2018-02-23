e687a298d687a21acff134354bf4ca1535cc3256
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-02-23 11:46:49 -0400

    [ADJUST] cap1

--- alura-refatoracao/src/br/alura/refatoracao/cap1/GeradorDeNotaFiscal.java
+++ alura-refatoracao/src/br/alura/refatoracao/cap1/GeradorDeNotaFiscal.java
@@ -4,7 +4,14 @@ public class GeradorDeNotaFiscal {
 
 	public NotaFiscal gera(Fatura fatura) {
 
-		// calcula valor do imposto
+		NotaFiscal nf = geraNf(fatura);
+		new EnviadorDeEmail().enviaEmail(nf);
+		new NFDao().salvaNoBanco(nf);
+
+		return nf;
+	}
+
+	private NotaFiscal geraNf(Fatura fatura) {
 		double valor = fatura.getValorMensal();
 		double imposto = 0;
 		if(valor < 200) {
@@ -16,24 +23,7 @@ public class GeradorDeNotaFiscal {
 		else {
 			imposto = valor * 0.07;
 		}
-		
-		NotaFiscal nf = new NotaFiscal(valor, imposto);
 
-		// envia email
-		String msgDoEmail = "Caro cliente,<br/>";
-		msgDoEmail += "É com prazer que lhe avisamos que sua nota fiscal foi "
-				+ "gerada no valor de " + nf.getValorLiquido() + ".<br/>";
-		msgDoEmail += "Acesse o site da prefeitura e faça o download.<br/><br/>";
-		msgDoEmail += "Obrigado!";
-		
-		System.out.println(msgDoEmail);
-		
-		// salva no banco
-		String sql = "insert into notafiscal (cliente, valor)"+
-					 "values (?," + nf.getValorLiquido() + ")";
-		
-		System.out.println("Salvando no banco" + sql);
-
-		return nf;
+		return new NotaFiscal(valor, imposto);
 	}
 }
